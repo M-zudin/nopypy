a0200223643e203396cea65447d7d5f9b65a0b6e
M-zudin <mikzudin@gmail.com>
2024-11-23 16:17:52 +0300

    Add files via upload

---
+++ nopypy.py
@@ -0,0 +1,20 @@
+import random
+import colorama
+import secrets
+import pygame
+import tkinter
+import socket
+import pyAesCrypt
+import rsa
+import math
+import os
+import shutil
+import time
+import turtle
+import keyboard
+import smtplib
+import subprocess
+os.system('cls')
+a=open(input('Path to execute: '))
+exec(a.read())
+a.close()
